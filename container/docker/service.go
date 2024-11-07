9123849e2db0ca4f075143e177e57e3bb981af73
jnbdz <237008+jnbdz@users.noreply.github.com>
2024-11-06 21:15:07 -0500

    Made some correction to package manager for windows and added a bunch of mocks

--- container/docker/service.go
+++ container/docker/service.go
@@ -1,6 +1,6 @@
 package docker
 
-// NewDockerService to set up the apt service
+// NewDockerService to set up the docker service
 func NewDockerService() IDocker {
 	return &SDocker{}
 }
--- entity/entity.go
+++ entity/entity.go
@@ -1,1 +1,5 @@
 package entity
+
+type IEntity interface{}
+
+type SEntity struct{}
